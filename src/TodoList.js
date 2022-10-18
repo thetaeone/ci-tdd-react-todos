9aee0ecbb05387a1243baccd628b1f944c7853cf
taeone <taewon1554@gmail.com>
2022-10-18 10:07:00 +0900

    TodoList TodoItem onToggle onRemove test OK

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-const Todolist =({todos})=>{
+const Todolist =({todos, onToggle, onRemove})=>{
 
     return(
         <ul>
             {todos.map(todo =>(
-                <TodoItem todo={todo} key={todo.id} />
+                <TodoItem todo={todo} key={todo.id} onToggle={onToggle} onRemove={onRemove} />
             ))}
         </ul>
     )
--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import Todolist from "./TodoList";
+import TodoList from "./TodoList";
 
 describe('<TodoList />',()=>{
     const sampleTodos =[
@@ -16,8 +16,19 @@ describe('<TodoList />',()=>{
         }
     ];
     it('has two todos',() =>{
-        const {getByText}=render(<Todolist todos={sampleTodos} />);
+        const {getByText}=render(<TodoList todos={sampleTodos} />);
         getByText(sampleTodos[0].text);
         getByText(sampleTodos[1].text);
     })
+    it('calls on Toggle and onRemove', () => {
+        const onToggle = jest.fn();
+        const onRemove = jest.fn();
+        const {getByText, getAllByText} = render (<TodoList todos={sampleTodos}
+            onToggle = {onToggle} onRemove = {onRemove} />);
+        fireEvent.click(getByText(sampleTodos[0].text));
+        expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+        fireEvent.click(getAllByText('삭제')[0]);
+        expect(onRemove).toBeCalledWith(sampleTodos[0].id);
+        
+    });
 })
